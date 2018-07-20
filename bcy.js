57d1a9793acca4a4f5caea2a603f56e97158875a
fga123bus <38732841+fga123bus@users.noreply.github.com>
2018-07-21 03:09:34 +0900

    1
    1

--- bcy.js
+++ bcy.js
@@ -132,7 +132,7 @@ var positions = [
         {
         title:'7-1.영등포농협본점',
         content :'<a class="arrow_box" data-toggle="modal" href="#myModal71">7-1.영등포농협본점</a>',
-        latlng: new daum.maps.LatLng(37.5108566,126.9126887)
+        latlng: new daum.maps.LatLng(37.511072, 126.912686)
         },
 		{
         title:'8.대신시장',
