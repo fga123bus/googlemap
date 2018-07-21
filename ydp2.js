5b62db84e12357f53943ba7818b71c3761d57000
fga123bus <38732841+fga123bus@users.noreply.github.com>
2018-07-22 00:13:36 +0900

    1
    1

--- ydp2.js
+++ ydp2.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new daum.maps.LatLng(37.515537,126.83979), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        level: 7 // 지도의 확대 레벨
     };
 
 var linePath = [
